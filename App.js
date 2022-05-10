4b4c0e5c6c427a2b393a5cdda392e59fa3e5bfd8
Andrés Sarmiento <acamilo707@gmail.com>
2022-05-10 16:56:08 -0500

    Changed the background color of the app

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
+import {StatusBar} from 'expo-status-bar';
+
 import Categories from "./screens/Categories";
 
 export default function App() {
     return (
         <>
+            <StatusBar style="light"/>
             <Categories/>
         </>
     );
