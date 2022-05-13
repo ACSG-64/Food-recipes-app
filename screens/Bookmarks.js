8f1d93ea40338edba8d69c6dad71fb1d2c3182de
Andrés Sarmiento <acamilo707@gmail.com>
2022-05-13 16:25:12 -0500

    Added the 'Bookmarks' screen component

---
+++ screens/Bookmarks.js
@@ -0,0 +1,8 @@
+import FoodList from "../components/FoodList/FoodList";
+
+// TODO: Get data from a 'store'
+export default function Bookmarks() {
+    return(
+        <FoodList data={[]} />
+    );
+}
